54d0f3036790b76b7aded20b58ebc4ddcfea9f45
Gabriel Moura <gmoura96@icloud.com>
2024-05-23 11:22:02 -0300

    fix ternary helper

--- pkg/ternary/ternary.go
+++ pkg/ternary/ternary.go
@@ -1,5 +1,6 @@
 package ternary
 
+// Or returns the first non-nil value, or the second value if both are nil.
 func Or[T any](a, b T) T {
 	if a != nil {
 		return a
@@ -7,13 +8,15 @@ func Or[T any](a, b T) T {
 	return b
 }
 
-func And(a, b interface{}) interface{} {
+// And returns the second value if the first value is non-nil, or nil if the first value is nil.
+func And[T any](a, b *T) *T {
 	if a == nil {
 		return nil
 	}
 	return b
 }
 
+// Ternary returns the first value if the statement is true, or the second value if the statement is false.
 func Ternary[T any](statement bool, a, b T) T {
 	if statement {
 		return a
